32c34cd0e250993d2972653b1ec65243135010cf
danieldigitalbo <danieldigitalbo@gmail.com>
2026-04-21 12:02:20 -0400

    Short code

--- addons/state_machine_bot/StateMachine/state.gd
+++ addons/state_machine_bot/StateMachine/state.gd
@@ -1,9 +1,9 @@
 extends Node
 class_name State
 
-@onready var state_machine:StateMachine = owner.find_child("*",true,false) as StateMachine
+@onready var state_machine:StateMachine = get_parent() as StateMachine
 
-func enter():pass
-func update(_delta:float):pass
-func update_physics(_delta:float):pass
-func exit():pass
+func enter() -> void: pass
+func update(_delta:float) -> void: pass
+func update_physics(_delta:float) -> void: pass
+func exit() -> void: pass
